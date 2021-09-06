236ee7f37df9252251d52054716bf7128b996b57
Aaronearlerichardson <jakdaxter31@gmail.com>
2021-09-06 12:18:32 -0400

    added rounding per class example

--- output_disp.py
+++ output_disp.py
@@ -2,5 +2,5 @@ weight = 20/2.075
 dosage = weight * 30
 
 print("Correct Dosage")
-print("for a patient weighing {} kg".format(weight))
-print("the correct dosage is {} mg the first day".format(dosage))
+print("for a patient weighing {} kg".format(round(weight)))
+print("the correct dosage is {} mg the first day".format(round(dosage)))
