510d6b963b930393d03cc622c6f4b82f93933e46
Aaron <jakdaxter31@gmail.com>
2021-09-13 14:44:58 -0400

    Finished and fixed testing

--- test_temp_calc.py
+++ test_temp_calc.py
@@ -1,7 +1,9 @@
 import pytest
 
 
-@pytest.mark.parameterize("my_input, expected", [([96, 96.5, 101.7, 98.7], True)])
+@pytest.mark.parametrize("my_input, expected", [
+    ([96, 96.5, 101.7, 98.7], True),
+    ([96, 96.5, 99.0, 98.7], False)])
 def test_detect_fever(my_input, expected):
     from temp_calc import detect_fever
     answer = detect_fever(my_input)
